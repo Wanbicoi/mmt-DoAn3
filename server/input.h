6b81743af03d6cecf53502d56fb8adba136879f1
huynhlonguni <106131819+huynhlonguni@users.noreply.github.com>
2023-05-18 22:31:29 +0700

    Debug

--- server/input.h
+++ server/input.h
@@ -1,6 +1,6 @@
 #include <windows.h>
 
-void mouse_move(float x, float y, int width, int height) {
+int mouse_move(float x, float y, int width, int height) {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
@@ -8,79 +8,81 @@ void mouse_move(float x, float y, int width, int height) {
 	input.mi.dy = (int) (y / (float)height * 65535);
 	input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_left_down() {
+int mouse_left_down(float x, float y, int width, int height) {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
-	input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	input.mi.dx = (int) (x / (float)width * 65535);
+	input.mi.dy = (int) (y / (float)height * 65535);
+	input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE | MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_left_up() {
+int mouse_left_up() {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
 	input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_middle_down() {
+int mouse_middle_down() {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
 	input.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_middle_up() {
+int mouse_middle_up() {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
 	input.mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_right_down() {
+int mouse_right_down() {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
 	input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_right_up() {
+int mouse_right_up() {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
 	input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_wheel_v(int delta) {
+int mouse_wheel_v(int delta) {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
 	input.mi.mouseData = delta;
 	input.mi.dwFlags = MOUSEEVENTF_WHEEL;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
 
-void mouse_wheel_h(int delta) {
+int mouse_wheel_h(int delta) {
 	INPUT input = {0};
 
 	input.type = INPUT_MOUSE;
 	input.mi.mouseData = delta;
 	input.mi.dwFlags = MOUSEEVENTF_HWHEEL;
 
-	SendInput(1, &input, sizeof(INPUT));
+	return SendInput(1, &input, sizeof(INPUT));
 }
